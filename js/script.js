49d03f10f3b147c5ce9c8017fac303d83257c520
Gabriel Souza <137961511+kort05@users.noreply.github.com>
2023-08-16 23:25:35 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//mostrar senha
+
+function show(){
+    let pwd = document.getElementById("pwd");
+    if(pwd.type == "password"){
+        pwd.type = "text";
+    } else{
+        pwd.type = "password";
+    }
+}
+
+//menu sanduiche
+
+
